3f04ab3779dded0964b3db0f994ef70c66ba6219
MC0803 <manasichoubey4@gmail.com>
2025-05-15 13:24:49 +0530

    commit -m"Patterns"

---
+++ javafullstack/javascript.js
@@ -0,0 +1,165 @@
+// JavaScript code
+   /* <script>
+
+   var x = 10;
+   var y = 20;
+   var z=x+y;
+ 
+   document.write(z);
+
+   </script>  */
+
+// Java local variable
+
+   /*<script> function abc(){
+
+   var x=10;//local variable
+
+   }
+
+   </script>  */
+
+// JavaScript global variable
+
+   /* <script>
+
+   var data=200;//gloabal variable function a(){ document.writeln(data);
+
+   }
+
+   function b(){ document.writeln(data);
+
+   }
+
+   a();//calling JavaScript function b();
+
+ 
+
+   </script>  */
+
+// Javascript types 
+   /* var a=40;holding number var b="Rahul";holding string
+   dyanmic type allocation
+   var c=true;holding boolean
+   */
+
+// JavaScript operators
+    /* var a=10; var b=20; var c=a+b; document.write(c); //addition
+    var a=10; var b=20; var c=a-b; document.write(c); //subtraction
+    var a=10; var b=20; var c=a*b; document.write(c); //multiplication
+    var a=10; var b=20; var c=a/b; document.write(c); //division
+    var a=10; var b=20; var c=a%b; document.write(c); //modulus
+
+    The + can also be used to add (concatenate) strings
+    Adding two numbers, will return the sum as a number like 5 + 5 = 10.
+
+    Adding a number and a string, will return the sum as a concatenated string like 5 + "5" = "55".
+    */
+
+// JavaScript increment and decrement
+   /* var a=10; document.write(a++); //post increment   
+    var a=10; document.write(++a); //pre increment
+    var a=10; document.write(a--); //post decrement
+    var a=10; document.write(--a); //pre decrement
+    */
+
+// JavaScript comparison operators
+    /* var a=10; var b=20; document.write(a==b); //equal to
+     var a=10; var b=20; document.write(a!=b); //not equal to
+     var a=10; var b=20; document.write(a>b); //greater than
+     var a=10; var b=20; document.write(a<b); //less than
+     var a=10; var b=20; document.write(a>=b); //greater than or equal to
+     var a=10; var b=20; document.write(a<=b); //less than or equal to
+
+     All the comparison operators above can also be used on strings
+     */
+
+// JavaScript logical operators
+    /* var a=true; var b=false; document.write(a&&b); //and operator
+     var a=true; var b=false; document.write(a||b); //or operator
+     var a=true; var b=false; document.write(!a); //not operator
+     */
+
+// JavaScript conditional operators
+    /*var a=10;
+    var b=(a>5)? "Greater than 5" : "Less than 5";
+    document.write(b);
+    */
+// JavaScript bitwise operators
+    /* var a=10; var b=20; document.write(a&b); //bitwise and
+     var a=10; var b=20; document.write(a|b); //bitwise or
+     var a=10; var b=20; document.write(a^b); //bitwise xor
+     var a=10; document.write(~a); //bitwise not
+     var a=10; var b=2; document.write(a<<b); //left shift
+     var a=10; var b=2; document.write(a>>b); //right shift
+     */
+
+
+// JavaScript if else statement
+    /* var a=10;
+    if(a>5){
+        document.write("a is greater than 5");
+    }
+    else{
+        document.write("a is less than 5");
+    }
+    */
+
+// JavaScript switch statement
+    /* var a=10;
+    switch(a){
+        case 5:
+            document.write("a is 5");
+            break;
+        case 10:
+            document.write("a is 10");
+            break;
+        default:
+            document.write("a is not 5 or 10");
+    }
+    */
+
+// JavaScript for loop
+      /* for(var i=0; i<5; i++){
+            document.write(i);
+        }
+        */
+
+// JavaScript while loop
+       /* var i=0;
+        while(i<5){
+            document.write(i);
+            i++;
+        }
+        */
+ 
+// JavaScript do while loop
+       /* var i=0;
+        do{
+            document.write(i);
+            i++;
+        }while(i<5);
+        */
+       
+// JavaScript break statement
+    /* for(var i=0; i<5; i++){
+     if(i==3){
+        break;
+     }
+     document.write(i);
+   }
+   */
+
+// JavaScript continue statement    
+    /* for(var i=0; i<5; i++){
+      if(i==3){
+          continue;
+      }
+      document.write(i);
+    }
+    */
+ 
+    
+        
+
+
