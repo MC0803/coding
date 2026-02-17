da236444777db87b62ce9ad01320f3cd484808b1
Khyati Choubey <manasichoubey4@gmail.com>
2026-02-17 23:00:57 +0530

    17-02-2026

---
+++ mycode/setMatrixZeros.cpp
@@ -0,0 +1,33 @@
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<int> rows(m, 0);
+    vector<int> cols(n, 0);
+
+    // Step 1: mark rows & cols
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                rows[i] = 1;
+                cols[j] = 1;
+            }
+        }
+    }
+
+    // Step 2: set zeros
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (rows[i] || cols[j])
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
