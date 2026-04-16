b084fef48fa4a33992d94959a62964138e3ec1c0
Khyati Choubey <manasichoubey4@gmail.com>
2026-04-16 10:50:51 +0530

    16-04-2026

--- mycode/LinkedListRemoveCycle.cpp
+++ mycode/LinkedListRemoveCycle.cpp
@@ -29,13 +29,53 @@ public:
         Node *newNode = new Node(val);
         if (head == NULL)
         {
-            newNode = head = tail;
+            head = tail = newNode;
         }
         else
         {
             newNode->next = head;
             head = newNode;
         }
+    }
+
+    void remCycle(Node *slow)
+    {
+        Node *fast = slow;
+        slow = head;
+        // special case of cycle at head
+        if (slow == fast)
+        {
+            while (fast->next != slow)
+            {
+                fast = fast->next;
+            }
+            fast->next = NULL;
+        }
+        Node *prev = fast;
+        while (slow != fast)
+        {
+            prev = fast;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        prev->next = NULL;
+    }
+
+    bool detectCycle()
+    {
+        Node *slow = head;
+        Node *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                remCycle(slow);
+                return true;
+            }
+        }
+        return false;
     }
     void printList()
     {
@@ -58,5 +98,7 @@ int main()
     ll.push_front(2);
     ll.push_front(1);
     ll.tail->next = ll.head->next; // create a cycle
+    if (ll.detectCycle())
+        cout << "Cycle detected and removed\n";
     ll.printList();
 }
